2887d1fc9cfe8c02c867f5e338c864100e92f3a6
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-01-07 22:37:03 +0300

    Ok

--- src/pages/Documentation/index.jsx
+++ src/pages/Documentation/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./../../components/Sidebar/index";
+import "./stylee.scss";
 
 const index = () => {
   return (
@@ -7,10 +8,19 @@ const index = () => {
       <div className="documentation">
         <div className="container">
           <div className="document">
-          <Sidebar />
-          <div className="document__right">
-            
-          </div>
+            <Sidebar />
+            <div className="document__right">
+              <div className="document__right--top">
+                <h3 className="fw-bold text-black">.NET Uzbekistan</h3>
+                <p className="doc__text fw-bold text-secondary my-3">
+                  Microsoft .NET platformasi bo'yicha O'zbekistonda ilk
+                  dokumentatsiya
+                </p>
+                <div className="doc_read">
+
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
